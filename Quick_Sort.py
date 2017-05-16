b0d880ce2aa1831ae7038cbe89edcc6e51a8d9eb
Tianyi Yang <tianyi.yang-ou@outlook.com>
2017-05-15 20:26:42 -0500

    feat: Upload Python code
    Upload Python implementation of Quick Sort

---
+++ Quick_Sort.py
@@ -0,0 +1,23 @@
+"""Implement quick sort in Python.
+Input a list.
+Output a sorted list."""
+def quicksort(array):
+    less=[]
+    equal=[]
+    greater=[]
+
+    if len(array)>1:
+        pivot=array[0]
+        for x in array:
+            if x<pivot:
+                less.append(x)
+            if x==pivot:
+                equal.append(x)
+            if x>pivot:
+                greater.append(x)
+        return quicksort(less)+equal+quicksort(greater)
+    else:
+        return array
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print quicksort(test)
