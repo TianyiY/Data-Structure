01792782b2bbb117f70135ee7734ef5cdded6414
Tianyi Yang <tianyi.yang-ou@outlook.com>
2017-10-16 04:06:34 -0500

    feat: Upload python

---
+++ Clustering_Coefficient.py
@@ -0,0 +1,33 @@
+flights=[("ORD", "SEA"), ("ORD", "LAX"), ("ORD", "DFW"), ("ORD", "PIT"),
+         ("SEA", "LAX"), ("LAX", "DFW"), ("DFW", "ATL"), ("ATL", "PIT"),
+         ("ATL", "RDU"), ("RDU", "PHL"), ("PHL", "PIT"), ("PHL", "PVD")]
+
+def make_link(G, node1, node2):
+    if node1 not in G:
+        G[node1] = {}
+    (G[node1])[node2] = 1
+    if node2 not in G:
+        G[node2] = {}
+    (G[node2])[node1] = 1
+    return G
+
+G={}
+for (x, y) in flights:
+    make_link(G, x, y)
+
+def clustering_coefficient(G, v):
+    neighbors=G[v].keys()
+    if len(neighbors)==1:
+        return 0.0
+    links=0
+    for w in neighbors:
+        for u in neighbors:
+            if u in G[w]:
+                links+=0.5
+    return 2.0*links/(len(neighbors)*(len(neighbors)-1))
+
+total=0
+for v in G.keys():
+    total+=clustering_coefficient(G, v)
+
+print total/len(G)
